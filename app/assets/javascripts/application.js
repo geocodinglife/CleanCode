7e66766c1c6e15019d72c693efbcacaa58cec6fb
ricardo alarcon <ricardo.alarcon@mobomo.com>
2020-12-20 19:01:22 -0500

    Refactor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,72 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+  //
+  //
+
+
+
+// this is to refactor the bottom part
+let twenty = 20
+let thirty = 30
+let number = 100
+let count = 0
+let last_number = 400
+let numbers = []
+    
+while(number < last_number) {
+  if (count < 1) {
+    numbers.push([number, number + thirty])
+    number += thirty
+    count = 1
+   } else {
+    numbers.push([number, number + twenty])
+    number += twenty
+    count = 0
+  }
+}
+   
+numbers.forEach((number) => {
+  if (innerHeight > 80 && innerHeight <= 100){
+    index = counting
+  } else if (innerHeight > number[0] && innerHeight <= number[1]) {
+    index = counting + 2
+  } else {
+    const innerWidth = getIndexFromWidth(width, Index)
+    index = innerWidth
+  }
+})
+
+return index
+
+
+// let index = 100
+// if (innerHeight > 80 && innerHeight <= 100) {
+//   index = 5
+// } else if (innerHeight > 100 && innerHeight < 130) {
+//   index = 7
+// } else if (innerHeight > 130 && innerHeight < 150) {
+//   index = 9
+// } else if (innerHeight > 150 && innerHeight < 180) {
+//   index = 11 
+// } else if (innerHeight > 180 && innerHeight < 200) {
+//   index = 13 
+// } else if (innerHeight > 200 && innerHeight < 230) {
+//   index = 15 
+// } else if (innerHeight > 230 && innerHeight < 260) {
+//   index = 17 
+// } else if (innerHeight > 260 && innerHeight < 290) {
+//   index = 19 
+// } else if (innerHeight > 290 && innerHeight < 310) {
+//   index = 21 
+// } else if (innerHeight > 340 && innerHeight < 340) {
+//   index = 23 
+// } else if (innerHeight > 370 && innerHeight < 400) {
+//   index = 25 
+// } else if (innerHeight > 130 && innerHeight < 150) {
+//   index = 27 
+//  else {
+//   const innerHeight = getIndexFromWidth(width, Index) 
+//    index = innerWidth
+//  }
+// return index
